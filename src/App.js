25805c4cfdaf6bd9110410d779ca077f85a1d610
James Dela Cruz <delacruzjamesmartin@gmail.com>
2021-02-27 14:46:14 +0800

    Added modal

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import { listLists } from "./graphql/queries";
 import "semantic-ui-css/semantic.min.css";
 import MainHeader from "./components/headers/MainHeader";
 import Lists from "./components/list/Lists";
-import { Button, Container, Icon } from "semantic-ui-react";
+import { Button, Container, Icon, Modal } from "semantic-ui-react";
 Amplify.configure(awsConfig);
 
 function App() {
   const [lists, setLists] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   async function fetchList() {
     const { data } = await API.graphql(graphqlOperation(listLists));
     setLists(data.listLists.items);
@@ -20,18 +21,34 @@ function App() {
   useEffect(() => {
     fetchList();
   }, []);
+
+  function toggleModal(action) {
+    setIsModalOpen(action);
+  }
   return (
     <AmplifyAuthenticator>
-      <AmplifySignOut />
-      <Button className="floatingButton">
-        <Icon name="plus" className="floatingButton_icon"></Icon>
-      </Button>
-      <Container>
+      <Container style={{ height: "100vh" }}>
+        <AmplifySignOut />
+        <Button className="floatingButton" onClick={() => toggleModal(true)}>
+          <Icon name="plus" className="floatingButton_icon"></Icon>
+        </Button>
         <div className="App">
           <MainHeader />
           <Lists lists={lists} />
         </div>
       </Container>
+      <Modal open={isModalOpen} dimmer={"blurring"}>
+        <Modal.Header>Create your list</Modal.Header>
+        <Modal.Content>Form to be added later</Modal.Content>
+        <Modal.Actions>
+          <Button negative onClick={() => toggleModal(false)}>
+            Cancel
+          </Button>
+          <Button positive onClick={() => toggleModal(false)}>
+            Save
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </AmplifyAuthenticator>
   );
 }
