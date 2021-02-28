ba9ddd0e04d90f659708d589a49f62882dc74998
James Dela Cruz <delacruzjamesmartin@gmail.com>
2021-02-28 23:41:22 +0800

    Added spinner

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,26 +1,37 @@
 import { Storage } from "aws-amplify";
 import React, { useEffect, useState } from "react";
-import { Icon, Item } from "semantic-ui-react";
+import { Dimmer, Icon, Item, Image, Loader } from "semantic-ui-react";
 
 function List(props) {
   const { title, description, imageKey, createdAt, dispatch } = props;
   const [imageUrl, setImageUrl] = useState(
     "https://react.semantic-ui.com/images/wireframe/image.png"
   );
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchImageUrl() {
     const imgUrl = await Storage.get(imageKey);
     setImageUrl(imgUrl);
+    setIsLoading(false);
   }
 
   useEffect(() => {
     if (imageKey) {
-      fetchImageUrl();
+      return fetchImageUrl();
     }
+    setIsLoading(false);
   }, []);
+
+  const content = <Loader />;
   return (
     <Item>
-      <Item.Image size="tiny" src={imageUrl} />
+      <Dimmer.Dimmable
+        dimmed={isLoading}
+        dimmer={{ active: isLoading, content }}
+        as={Image}
+        size="tiny"
+        src={imageUrl}
+      ></Dimmer.Dimmable>
       <Item.Content>
         <Item.Header>{title}</Item.Header>
         <Item.Description>{description}</Item.Description>
