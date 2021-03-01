ca6faa4602847c517601aeb416bedde4dbbde594
James Dela Cruz <delacruzjamesmartin@gmail.com>
2021-03-01 21:10:24 +0800

    Implement list page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./Main";
+import List from "./List";
 
 function App(params) {
   return (
     <BrowserRouter>
       <Switch>
+        <Route
+          path="/list/:slug"
+          render={(props) => {
+            return <List {...props} />;
+          }}
+        />
         <Route path="/">
-          <Main />
-        </Route>
-        <Route path="/list">
           <Main />
         </Route>
       </Switch>
---
+++ src/List.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function List(props) {
+  console.log(props);
+  return <h1>List</h1>;
+}
+
+export default List;
